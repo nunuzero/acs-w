94e9b74b941dc543e48d7bbf426429ea16e50262
Anugerah Ashar <nunuzerosos@gmail.com>
2025-04-03 06:41:35 +0800

    add filament admin panel

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            ['name' => 'Admin', 'email' => 'admin@gmail.com', 'password' => 'password'],
+            ['name' => 'User', 'email' => 'user@gmail.com', 'password' => 'password'],
+        ];
+
+        User::factory()->createMany($users);
+    }
+}
